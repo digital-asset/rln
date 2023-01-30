d7343c68e13fada953579b9e6ed1bc8d1fe3a0f3
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2023-01-30 16:49:27 +0100

    Added no-arg constructor to generated files.

--- gui-backend/src/main/java/com/rln/gui/backend/implementation/methods/RemoteOwnedAddressSupplier.java
+++ gui-backend/src/main/java/com/rln/gui/backend/implementation/methods/RemoteOwnedAddressSupplier.java
@@ -2,7 +2,7 @@ package com.rln.gui.backend.implementation.methods;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rln.gui.backend.implementation.config.WalletAddress;
+import com.rln.gui.backend.model.WalletAddressDTO;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
@@ -10,18 +10,18 @@ import java.util.List;
 public class RemoteOwnedAddressSupplier {
 
   private final Path remoteOwnedAddressConfig;
-  private final List<WalletAddress> remoteOwnedAddresses;
+  private final List<WalletAddressDTO> remoteOwnedAddresses;
 
   public RemoteOwnedAddressSupplier(Path remoteOwnedAddressConfig) {
     this.remoteOwnedAddressConfig = remoteOwnedAddressConfig;
     this.remoteOwnedAddresses = readRemoteOwnedAddresses();
   }
 
-  public List<WalletAddress> getRemoteOwnedAddresses() {
+  public List<WalletAddressDTO> getRemoteOwnedAddresses() {
     return remoteOwnedAddresses;
   }
 
-  private List<WalletAddress> readRemoteOwnedAddresses() {
+  private List<WalletAddressDTO> readRemoteOwnedAddresses() {
     try {
       return new ObjectMapper().readValue(
           remoteOwnedAddressConfig.toFile(),
