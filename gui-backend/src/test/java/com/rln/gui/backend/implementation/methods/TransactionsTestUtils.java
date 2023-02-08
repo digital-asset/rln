8db1343c8b7b22e80fa4cc45ef6a93b26f66f885
Gabor Hosszu <gabor@digitalasset.com>
2023-02-08 10:16:26 +0100

    Fix tests (txId)

--- gui-backend/src/test/java/com/rln/gui/backend/implementation/methods/TransactionsTestUtils.java
+++ gui-backend/src/test/java/com/rln/gui/backend/implementation/methods/TransactionsTestUtils.java
@@ -71,7 +71,7 @@ public class TransactionsTestUtils {
                                                   String iban, Subject subject, String status, String bic, String assetCode,
                                                   BigDecimal transactionAmount) {
     Assertions.assertEquals(CompoundUniqueIdUtil.getCompoundUniqueId(subject, contractId), listedItem.getId());
-    Assertions.assertTrue(listedItem.getTransactionId().isEmpty());
+    Assertions.assertEquals(groupId, listedItem.getTransactionId());
     Assertions.assertEquals(groupId, listedItem.getGroupId());
     Assertions.assertEquals(messageId, listedItem.getMessageId());
     Assertions.assertEquals(iban, listedItem.getAddress());
