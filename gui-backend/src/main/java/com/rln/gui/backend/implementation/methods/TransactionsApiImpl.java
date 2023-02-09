391aec7c425581e79de8336ad1c6cf53e9408023
Gabor Hosszu <gabor@digitalasset.com>
2023-02-09 14:28:31 +0100

    Synchronize on the map

--- gui-backend/src/main/java/com/rln/gui/backend/implementation/methods/TransactionsApiImpl.java
+++ gui-backend/src/main/java/com/rln/gui/backend/implementation/methods/TransactionsApiImpl.java
@@ -31,87 +31,89 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 public class TransactionsApiImpl {
-  // GUI Backend is run for a Daml participant, it communicates directly with the ledger, so it settles on the ledger
-  private final boolean GUI_BACKEND_SETTLES_ON_LEDGER = true;
-  private final RandomShardPartyPicker schedulerRandomShardPartyPicker;
-  private final GuiBackendConfiguration guiBackendConfiguration;
-  private final TransferProposalRepository transferProposals;
-  private final RLNClient rlnClient;
-  private final TransferProposalToApiTypeConverter converter;
-  
-  private final ConcurrentHashSet<String> alreadyApprovedTransferProposals = new ConcurrentHashSet<String>();
+    // GUI Backend is run for a Daml participant, it communicates directly with the ledger, so it settles on the ledger
+    private final boolean GUI_BACKEND_SETTLES_ON_LEDGER = true;
+    private final RandomShardPartyPicker schedulerRandomShardPartyPicker;
+    private final GuiBackendConfiguration guiBackendConfiguration;
+    private final TransferProposalRepository transferProposals;
+    private final RLNClient rlnClient;
+    private final TransferProposalToApiTypeConverter converter;
 
-  public TransactionsApiImpl(RandomShardPartyPicker schedulerRandomShardPartyPicker,
-                             GuiBackendConfiguration guiBackendConfiguration,
-                             TransferProposalToApiTypeConverter converter,
-                             TransferProposalRepository transferProposals,
-                             RLNClient rlnClient) {
-    this.schedulerRandomShardPartyPicker = schedulerRandomShardPartyPicker;
-    this.guiBackendConfiguration = guiBackendConfiguration;
-    this.transferProposals = transferProposals;
-    this.rlnClient = rlnClient;
-    this.converter = converter;
-  }
+    private final ConcurrentHashSet<String> alreadyApprovedTransferProposals = new ConcurrentHashSet<String>();
 
-  public Object updateTransactionApprovalStatus(
-      @Valid TransactionStatusUpdate transactionStatusUpdate) {
-    var contractId = CompoundUniqueIdUtil.parseSubjectAndContractId(transactionStatusUpdate.getId())._2;
-    if (!alreadyApprovedTransferProposals.contains(contractId)) {
-      var approved = transactionStatusUpdate.getStatus().equals(StatusEnum.APPROVE);
-      ApproveRejectProposalChoiceParameters parameters =
-              new ApproveRejectProposalChoiceParameters(
-                      guiBackendConfiguration.partyDamlId(),
-                      new ContractId(contractId),
-                      approved,
-                      GuiBackendConstants.DEFAULT_GUI_BACKEND_REASON,
-                      GUI_BACKEND_SETTLES_ON_LEDGER);
-      rlnClient.exerciseApproveRejectProposalChoice(transactionStatusUpdate.getId(), parameters);
-      alreadyApprovedTransferProposals.add(contractId);
+    public TransactionsApiImpl(RandomShardPartyPicker schedulerRandomShardPartyPicker,
+                               GuiBackendConfiguration guiBackendConfiguration,
+                               TransferProposalToApiTypeConverter converter,
+                               TransferProposalRepository transferProposals,
+                               RLNClient rlnClient) {
+        this.schedulerRandomShardPartyPicker = schedulerRandomShardPartyPicker;
+        this.guiBackendConfiguration = guiBackendConfiguration;
+        this.transferProposals = transferProposals;
+        this.rlnClient = rlnClient;
+        this.converter = converter;
     }
-    return Collections.emptyMap();
-  }
 
-  public List<Transaction> getRequiredApprovals() {
-    return transferProposals
-        .findAllWaiting().stream()
-        .map(converter::transferProposalToTransaction)
-        .collect(Collectors.toList());
-  }
+    public Object updateTransactionApprovalStatus(
+            @Valid TransactionStatusUpdate transactionStatusUpdate) {
+        var contractId = CompoundUniqueIdUtil.parseSubjectAndContractId(transactionStatusUpdate.getId())._2;
+        synchronized (alreadyApprovedTransferProposals) {
+            if (!alreadyApprovedTransferProposals.contains(contractId)) {
+                var approved = transactionStatusUpdate.getStatus().equals(StatusEnum.APPROVE);
+                ApproveRejectProposalChoiceParameters parameters =
+                        new ApproveRejectProposalChoiceParameters(
+                                guiBackendConfiguration.partyDamlId(),
+                                new ContractId(contractId),
+                                approved,
+                                GuiBackendConstants.DEFAULT_GUI_BACKEND_REASON,
+                                GUI_BACKEND_SETTLES_ON_LEDGER);
+                rlnClient.exerciseApproveRejectProposalChoice(transactionStatusUpdate.getId(), parameters);
+                alreadyApprovedTransferProposals.add(contractId);
+            }
+        }
+        return Collections.emptyMap();
+    }
 
-  public List<Transaction> getTransactions(Boolean incompleteOnly, String address, Long limit,
-      Long offset) {
-    return transferProposals
-        .findAll(byAddress(address).and(byIsWaiting(incompleteOnly)), limit, offset).stream()
-        .map(converter::transferProposalToTransaction)
-        .collect(Collectors.toList());
-  }
+    public List<Transaction> getRequiredApprovals() {
+        return transferProposals
+                .findAllWaiting().stream()
+                .map(converter::transferProposalToTransaction)
+                .collect(Collectors.toList());
+    }
 
-  private static Predicate<com.rln.gui.backend.ods.TransferProposal> byIsWaiting(Boolean incompleteOnly) {
-    return transferProposal -> !Objects.requireNonNullElse(incompleteOnly, false) || transferProposal.isWaiting();
-  }
+    public List<Transaction> getTransactions(Boolean incompleteOnly, String address, Long limit,
+                                             Long offset) {
+        return transferProposals
+                .findAll(byAddress(address).and(byIsWaiting(incompleteOnly)), limit, offset).stream()
+                .map(converter::transferProposalToTransaction)
+                .collect(Collectors.toList());
+    }
 
-  private static Predicate<com.rln.gui.backend.ods.TransferProposal> byAddress(String address) {
-    return transferProposal -> address == null || address.equals(transferProposal.getAddress());
-  }
+    private static Predicate<com.rln.gui.backend.ods.TransferProposal> byIsWaiting(Boolean incompleteOnly) {
+        return transferProposal -> !Objects.requireNonNullElse(incompleteOnly, false) || transferProposal.isWaiting();
+    }
 
-  public void updateApprovalProperties(@Valid ApprovalProperties approvalProperties) {
+    private static Predicate<com.rln.gui.backend.ods.TransferProposal> byAddress(String address) {
+        return transferProposal -> address == null || address.equals(transferProposal.getAddress());
+    }
 
-  }
+    public void updateApprovalProperties(@Valid ApprovalProperties approvalProperties) {
 
-  public void finalised(@Valid Finalised finalised) {
+    }
 
-  }
+    public void finalised(@Valid Finalised finalised) {
 
-  public List<Approval> transferProposal(@Valid TransferProposal transferProposal) {
-    rlnClient.createInitiateTransferContract(translateToInitiateTransfer(transferProposal));
-    return Collections.emptyList();
-  }
+    }
 
-  private InitiateTransfer translateToInitiateTransfer(TransferProposal createTransaction) {
-    var payload = createTransaction.getPayload();
-    var initiator = guiBackendConfiguration.partyDamlId();
-    var groupId = createTransaction.getGroupId();
-    var scheduler = schedulerRandomShardPartyPicker.pickRandomShardParty();
-    return new InitiateTransfer(groupId, initiator, scheduler, payload);
-  }
+    public List<Approval> transferProposal(@Valid TransferProposal transferProposal) {
+        rlnClient.createInitiateTransferContract(translateToInitiateTransfer(transferProposal));
+        return Collections.emptyList();
+    }
+
+    private InitiateTransfer translateToInitiateTransfer(TransferProposal createTransaction) {
+        var payload = createTransaction.getPayload();
+        var initiator = guiBackendConfiguration.partyDamlId();
+        var groupId = createTransaction.getGroupId();
+        var scheduler = schedulerRandomShardPartyPicker.pickRandomShardParty();
+        return new InitiateTransfer(groupId, initiator, scheduler, payload);
+    }
 }
